19e9ef90d216c9164acf77b2449c6bc2b5e89cf9
Renard <ludofernandez@msn.com>
2021-01-28 16:10:14 +0100

    Add eprint

---
+++ eprint.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Project by
+# Ludovic Fernandez
+# http://github.com/Wrexes
+
+# from __future__ import print_function
+import sys
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)

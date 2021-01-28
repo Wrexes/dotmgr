0a4be65654f3cfdad638a6b78da01025054ec546
Renard <ludofernandez@msn.com>
2021-01-28 18:07:19 +0100

    refactor: Separate config and dots modules

--- config.py
+++ config.py
@@ -6,52 +6,18 @@
 # Ludovic Fernandez
 # http://github.com/Wrexes
 
-import json
-import re
 import os
 
+from glob import glob as wildcard
 
-class Dot:
-    import eprint
+import defaults
 
-    # def __init__(self, Name=None, Command=None, Files=[], Ignores=None):
-    def __init__(self, Name=None, Command=None, Files=[]):
-        self.name = Name
-        self.command = Command
-        self.files = Files
-        # self.ignores = Ignores
-        # self.__validate()
+xdgConfDir = defaults.xdgConfDir
+confDir = defaults.confDir
 
-    def __str__(self) -> str:
-        ret = "<Name>\n\t{self.name}\n"
-        ret += "<Command>\n\t{self.command}\n"
-        ret += "<Files>\n\t{self.files}\n"
-        return ret.format(self=self)
 
-    def __validate(self):
-        reval = "^[a-zA-Z0-9_][a-zA-Z0-9_i\-]+$"
-        if self.name is None:
-            raise KeyError("Missing name property.")
-        if re.fullmatch(reval, self.Command) is None:
-            raise KeyError("Invalid App name, must match this regex: " + reval)
-        if self.command is None:
-            raise KeyError("Missing command property.")
-        if re.fullmatch(reval, self.Command) is None:
-            raise NameError(
-                "Invalid App command, must match this regex: " + reval)
-        if self.files.__len__() < 1:
-            raise KeyError("No config file specified for " + self.name)
-
-    @classmethod
-    def from_json_string(cls, json_string):
-        json_dict = json.loads(json_string)
-        return cls(**json_dict)
-
-    @classmethod
-    def from_json(cls, json_file):
-        try:
-            with open(json_file, "r") as jf:
-                return cls.from_json_string(jf.read())
-        except json.JSONDecodeError as e:
-            eprint("Error while parsing file <" + jf.name + ">: ")
-            exit(1)
+# Create missing directories
+if not os.path.exists(xdgConfDir):
+    os.mkdir(xdgConfDir)
+if not os.path.exists(confDir):
+    os.mkdir(confDir)

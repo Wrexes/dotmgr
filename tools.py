a0e5dc19e80d2329743da97d273dc52b636fe605
Renard <ludofernandez@msn.com>
2021-01-29 14:40:15 +0100

    refactor: Change eprint module to generic tools module

--- tools.py
+++ tools.py
@@ -8,6 +8,10 @@
 
 # from __future__ import print_function
 import sys
+import os
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+
+def realpath(path: str) -> str:
+    return os.path.expandvars(os.path.expanduser(path))

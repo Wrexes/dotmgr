7a3dca24c2bdec16bbae07f8109cb687b77b8fdb
Renard <ludofernandez@msn.com>
2021-02-12 19:48:49 +0100

    feat: Add cache method for save class
    Creates a cache directory with a "standardized" name containing
    dotmgr_[username]-[appname]-[confname] in the OS's temp directory.
    This cache will contain all the files to save plus a JSON file
    containing the necessary info to restore its contents.

--- commands.py
+++ commands.py
@@ -6,7 +6,13 @@
 # Ludovic Fernandez
 # http://github.com/Wrexes
 
+import os
+import shutil
+import json
+
 import dot
+import tools
+import config
 
 class list:
     @staticmethod
@@ -20,4 +26,35 @@ class list:
            print(dot.name)
 
 class save:
-    pass
+    @staticmethod
+    def cache(dot: dot, userName=config.userName, confName="default", cacheDir=config.cacheDir, override=False):
+        match = {}
+        cache = os.path.join(cacheDir, "dotmgr_" + userName + "-" + dot.command + "-" + confName)
+        if override is True and os.path.exists(cache):
+            shutil.rmtree(cache)
+        os.mkdir(cache)
+
+        # For every file/dir, create a copy in the cache and add where to restore it in the matching dict
+        for src in dot.include:
+            dst = os.path.join(cache, os.path.basename(src))
+            if os.path.isfile(dot.get_file_path(src)):
+                shutil.copy2(tools.realpath(src), dst)
+            else:
+                shutil.copytree(tools.realpath(src), dst)
+            match[os.path.basename(src)] = src
+
+        # Clean up ignored files and directories
+        for root, dirs, files in os.walk(cache):
+            if dot.is_excluded(root):
+                shutil.rmtree(root)
+            for f in files:
+                if dot.is_excluded(f):
+                    os.remove(f)
+
+        # Create a JSON matching file
+        with open(os.path.join(cache, "_dotmatch.json"), mode='wt') as f:
+            json.dump(match, f, sort_keys=True)
+
+    @staticmethod
+    def archive(path: str):
+        pass

dccf0bad42cf81b1e49063d32cd44b13e5cfbb18
Renard <ludofernandez@msn.com>
2021-02-16 16:49:38 +0100

    feat: Add name builder for caches in save class

--- commands.py
+++ commands.py
@@ -13,6 +13,8 @@ import json
 import dot
 import tools
 import config
+import tarfile
+
 
 class list:
     @staticmethod
@@ -22,14 +24,22 @@ class list:
 
     @staticmethod
     def installed():
-       for dot in dot.installed():
-           print(dot.name)
+        for dot in dot.installed():
+            print(dot.name)
+
 
 class save:
+
+    # Build the name string for a cache
+    @staticmethod
+    def cache_path(dot: dot, userName=config.userName, confName="default", cacheDir=config.cacheDir):
+        return os.path.join(cacheDir, "dotmgr_" + userName + "-" + dot.name + "-" + confName)
+
+    # Create a temporary directory containing a copy of the dotfiles for given dot
     @staticmethod
     def cache(dot: dot, userName=config.userName, confName="default", cacheDir=config.cacheDir, override=False):
         match = {}
-        cache = os.path.join(cacheDir, "dotmgr_" + userName + "-" + dot.command + "-" + confName)
+        cache = save.cache_path(dot, userName, confName, cacheDir)
         if override is True and os.path.exists(cache):
             shutil.rmtree(cache)
         os.mkdir(cache)
@@ -55,6 +65,9 @@ class save:
         with open(os.path.join(cache, "_dotmatch.json"), mode='wt') as f:
             json.dump(match, f, sort_keys=True)
 
+    # Insert a cache into the archive where every configuration is saved
     @staticmethod
-    def archive(path: str):
-        pass
+    def pack(cachePath: str, tarPath: str = config.tarPath):
+        cache = save.cache_path(dot, userName, confName, cacheDir)
+        with tarfile.open(os.path.join(config.cacheDir, "dotmgr.tar.gz"), 'a:gz') as tar:
+            pass

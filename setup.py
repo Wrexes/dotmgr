d0879d1518cdc7ba6df5e1a97d616a5e4e17a555
Renard <ludofernandez@msn.com>
2021-02-18 15:35:40 +0100

    feat: Add setup.py

---
+++ setup.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# MIT License
+
+# Copyright (c) 2020 Ludovic Fernandez
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from setuptools import setup
+from DotManager.defaults import version
+
+with open("README.md", 'r', encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="DotManager",
+    version=version,
+    description="A flexible dotfile manager written in pure-Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Sir Wrexes",
+    author_email="ludofernandez@msn.com",
+    url="https://github.com/SirWrexes/DotManager",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Utilities",
+        "Typing :: Typed",
+    ],
+    include_package_data=True,
+    # packages=["DotManager"],
+    # package_dir={"DotManager": "DotManager"},
+    # package_data={"DotManager": ["DotManager/dotinfo/*.dotinfo"]}
+    install_requires=["docopt"],
+    scripts=["DotManager/dot"]
+)

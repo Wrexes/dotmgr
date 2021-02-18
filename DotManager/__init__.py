26fcfb416643beec4ad91f38b4c42f20838efa3b
Renard <ludofernandez@msn.com>
2021-02-18 12:59:03 +0100

    chore: Insert licence in every source file
    Am I doing it right ?
    No but sincerely, am I doing it right ?
    This is a real question.
    Feel free to comment this commit with "yes" or "no".

---
+++ DotManager/__init__.py
@@ -0,0 +1,33 @@
+# MIT License
+
+# Copyright (c) 2020 Ludovic Fernandez
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+""" Apparently this __init__.py file is mandatory. 🤔
+    Oh well. I don't have any code to execute at load time (yet).
+    Anyway, this file is not interesting. Why are you still reading ?
+    I can go on and on for hours you know ?
+    Lorem ipsum sit dolor amet- No I'm kidding, I wouldn't reduce myself
+    to such laziness.
+    I hope you're having a great time. Are you ? Plese tell me you are.
+    If not, then I *really* wonder why you keep reading.
+    Alright. I'm out of ideas... for now.
+    See ya !
+    """

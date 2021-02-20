d58fc9d6ee7287cfa454eeadbbde6c4b0b5e8f52
Renard <ludofernandez@msn.com>
2021-02-20 11:44:33 +0100

    chore: Update version number
    I always forget.

--- DotManager/defaults.py
+++ DotManager/defaults.py
@@ -29,7 +29,7 @@ import tempfile
 from pathlib import Path
 
 # DotManager version
-version = '0.3.1'
+version = '0.4.0'
 
 # Set the base directory for config files
 if "XDG_CONFIG_HOME" in os.environ:
